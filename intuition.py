a19440f2b9f8057bd91630ee300c896f01902bf1
freemesh2025 <2498065510@qq.com>
2025-04-01 11:22:15 +0800

    entropy

--- intuition.py
+++ intuition.py
@@ -3,19 +3,22 @@ import math
 
 
 def entropy(s):
-    total = 0
-    l = len(s)
+    naive_entropy = 0
     cnt = Counter(s) 
-    for k in cnt:
-        p = cnt[k] / l
-        total += -p * math.log2(p)
-    return total
+    for c,freq in cnt.items():
+        p = freq / len(s)
+        naive_entropy += -p * math.log2(p)
+    return naive_entropy
 
 def avg_entropy(s, vocab):
-    original = entropy(s)
-    avg_l = sum([len(v) for v in vocab.values()]) / len(vocab)
-    
-    return original / avg_l
+    naive_entropy = 0
+    avg_l = 0
+    cnt = Counter(s)
+    for c,freq in cnt.items():
+        p = freq / len(s)
+        naive_entropy += -p * math.log2(p)
+        avg_l += p * len(vocab[c])
+    return naive_entropy / avg_l
 
 
 ################
@@ -40,7 +43,7 @@ print(entropy(A)) # 1.0
 print(entropy(B)) # 1.0
 
 print(avg_entropy(A, vocab_a)) # 1.0 
-print(avg_entropy(B, vocab_b)) # 0.5
+print(avg_entropy(B, vocab_b)) # 0.33
 
 ######################
 A = 'aadabbacc'
@@ -74,6 +77,7 @@ vocab_d ={
     'a': 'a',
     'b': 'b',
     'c': 'c',
+    'd': 'd',
     'X': 'aa',
     'Y': 'bb',
 }
@@ -81,6 +85,6 @@ vocab_d ={
 
 
 print(avg_entropy(A, vocab_a)) # baseline 1.837
-print(avg_entropy(B, vocab_b)) # + MC 1.875
+print(avg_entropy(B, vocab_b)) # + MC 2.0
 print(avg_entropy(C, vocab_c)) # + RAC 1.837  
-print(avg_entropy(D, vocab_d)) # + RMC 1.087 
+print(avg_entropy(D, vocab_d)) # + RMC 0.846
